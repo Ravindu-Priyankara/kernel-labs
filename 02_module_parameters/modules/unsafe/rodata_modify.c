1c2488b27388f7e1e5171127c7f6786a6d530f71
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-04-02 14:47:50 +0530

    feat: Add rodata modify kernel module

---
+++ 02_module_parameters/modules/unsafe/rodata_modify.c
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+* ================================================================
+*  Module: mod_param_str.c
+*
+*  Description:
+* A simple Linux Kernel Module that crashes your kernel. | Do not run ....!
+*
+*  Important Notes:
+*  - This program crashes your kernel because it tries to modify the .rodata section stored variable.
+*
+*  Purpose:
+*  Learning what happens if u try to change a pointer that was stored via rodata.
+*
+*  Author: Ravindu Priyankara
+*  Project: Kernel Lab
+*
+*  Kernel Version: Tested on Linux 6.17.0-19-generic
+*
+*  ================================================================
+*/
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/kgdb.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ravindu Priyankara");
+MODULE_DESCRIPTION("Simple Linux Kernel Module that panic your kernel.");
+MODULE_PARM_DESC(name, "String value");
+MODULE_VERSION("1.0");
+
+// stored .rodata
+static char *name = "Ravindu";
+
+static __init int panic_init(void){
+    kgdb_breakpoint();
+    pr_info("module loading...\n");
+    name[0] = 'K';  // crash{ try to modify .rodata stored string}
+    pr_info("changed...!\n");
+
+    return 0;
+}
+
+static __exit void panic_exit(void){
+    pr_info("bye");
+}
+
+module_init(panic_init);
+module_exit(panic_exit);
